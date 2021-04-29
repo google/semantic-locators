a6cd061f85d36b1dddfca03e266279b88177ed2d
Alex Lloyd <alexlloyd@google.com>
2021-04-29 05:00:28 -0700

    Add copyright headers to Java source files.
    and fix a typo
    
    PiperOrigin-RevId: 371097036

--- webdriver_java/src/test/java/com/google/semanticlocators/BySemanticLocatorTest.java
+++ webdriver_java/src/test/java/com/google/semanticlocators/BySemanticLocatorTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.semanticlocators;
 
 import static com.google.common.truth.Truth.assertThat;
@@ -111,7 +127,7 @@ public final class BySemanticLocatorTest {
 
   @Test
   @Parameters(method = "getAllDriverNames")
-  public void findElements_throwsExceptionForNoElementsFound(String driverName) throws Exception {
+  public void findElement_throwsExceptionForNoElementsFound(String driverName) throws Exception {
     WebDriver driver = getDriver(driverName);
     assertThrows(
         NoSuchElementException.class,
