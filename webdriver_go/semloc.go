0db2bfcf01f5def87ff6b7dadd7a4675edf45dd1
Googler <noreply@google.com>
2021-06-07 15:19:30 -0700

    Internal change
    PiperOrigin-RevId: 378017557

---
+++ webdriver_go/semloc.go
@@ -0,0 +1,146 @@
+// Package semloc provides facilities for locating web elements using semantic
+// locators. See http://github.com/google/semantic-locators#readme for more
+// info.
+//
+// Copyright 2021 The Semantic Locators Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package semloc
+
+import (
+	// Enable go:embed
+	_ "embed"
+	"errors"
+	"fmt"
+	"strings"
+
+	// copybara:strip_begin(replace_selenium_import)
+	"google3/third_party/golang/selenium/selenium"
+	// copybara:strip_end_and_replace_begin
+	// "github.com/tebeka/selenium"
+	// copybara:replace_end
+)
+
+//go:embed wrapper_bin.js
+var jsImplementation string
+
+func installJS(wd selenium.WebDriver) error {
+	installedRaw, err := wd.ExecuteScript("return window.semanticLocatorsReady === true;", nil)
+	if err != nil {
+		return fmt.Errorf("wd.ExecuteScript(isInstalled): %w", err)
+	}
+	installed, ok := installedRaw.(bool)
+	if !ok {
+		return fmt.Errorf("expected isInstalled script to return a bool, but got %T", installedRaw)
+	}
+	if installed {
+		return nil
+	}
+
+	_, err = wd.ExecuteScript(jsImplementation, nil)
+	return err
+}
+
+// interpretError takes in an error resulting from executing the semantic
+// locator javascript and re-interprets them.
+// In selenium, all errors resulting from running a javascript script have the
+// "javascript error" error code. However, it makes more sense for errors thrown
+// when there is no matching element to have the same "no such element" error
+// code that is returned by selenium.WebDriver.FindElement when it cannot find
+// an element. The same is performed for errors due to invalid locators.
+// See third_party/semantic_locators/javascript/lib/error.ts for semantic
+// locator javascript error message values. See
+// https://w3c.github.io/webdriver/#errors for webdriver error message values.
+func interpretError(err error) error {
+	var serr *selenium.Error
+	if !errors.As(err, &serr) {
+		return err
+	}
+	if serr.Err != "javascript error" {
+		return err
+	}
+
+	// Error messages are in the format
+	// "javascript error: <error name>: <human readable message>"
+	// we want to switch on the 2nd part (error name)
+	parts := strings.SplitN(serr.Message, ":", 3)
+	if len(parts) != 3 {
+		return err
+	}
+	errName := strings.TrimSpace(parts[1])
+
+	switch errName {
+	case "NoSuchElementError":
+		return &selenium.Error{
+			Err:        "no such element",
+			Message:    serr.Message,
+			Stacktrace: serr.Stacktrace,
+			HTTPCode:   404,
+			LegacyCode: serr.LegacyCode,
+		}
+	case "InvalidLocatorError":
+		return &selenium.Error{
+			Err:        "invalid selector",
+			Message:    serr.Message,
+			Stacktrace: serr.Stacktrace,
+			HTTPCode:   400,
+			LegacyCode: serr.LegacyCode,
+		}
+	default:
+		return err
+	}
+}
+
+// FindElement returns the first WebElement located by the given semantic
+// locator. If 'context' is non-nil, only elements that are descendants of
+// 'context' are considered.
+func FindElement(wd selenium.WebDriver, locator string, context selenium.WebElement) (selenium.WebElement, error) {
+	if err := installJS(wd); err != nil {
+		return nil, fmt.Errorf("installJS: %w", err)
+	}
+
+	args := []interface{}{locator}
+	if context != nil {
+		args = append(args, context)
+	}
+
+	elmRaw, err := wd.ExecuteScriptRaw("return window.findElementBySemanticLocator.apply(null, arguments)", args)
+	err = interpretError(err)
+	if err != nil {
+		return nil, fmt.Errorf("wd.ExecuteScriptRaw(window.findElementBySemanticLocator): %w", err)
+	}
+
+	return wd.DecodeElement(elmRaw)
+}
+
+// FindElements returns all the WebElements located by the given semantic
+// locator.  If 'context' is non-nil, only elements that are descendants of
+// 'context' are included.
+func FindElements(wd selenium.WebDriver, locator string, context selenium.WebElement) ([]selenium.WebElement, error) {
+	if err := installJS(wd); err != nil {
+		return nil, fmt.Errorf("installJS: %w", err)
+	}
+
+	args := []interface{}{locator}
+	if context != nil {
+		args = append(args, context)
+	}
+
+	elmsRaw, err := wd.ExecuteScriptRaw("return window.findElementsBySemanticLocator.apply(null, arguments)", args)
+	err = interpretError(err)
+	if err != nil {
+		return nil, fmt.Errorf("wd.ExecuteScriptRaw(window.findElementsBySemanticLocator): %w", err)
+	}
+
+	return wd.DecodeElements(elmsRaw)
+}
---
+++ webdriver_go/semloc_test.go
@@ -0,0 +1,225 @@
+// Copyright 2021 The Semantic Locators Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package semloc
+
+import (
+	"errors"
+	"testing"
+
+	// copybara:strip_begin(remove_google3_webtest_import)
+	"google3/testing/web/go/webtest"
+	// copybara:strip_end
+
+	// copybara:strip_begin(replace_selenium_import)
+	"google3/third_party/golang/selenium/selenium"
+	// copybara:strip_end_and_replace_begin
+	// "github.com/tebeka/selenium"
+	// copybara:replace_end
+)
+
+func newWebdriver(t *testing.T) selenium.WebDriver {
+	// copybara:strip_begin(webdriver_initialization)
+	wd, err := webtest.NewWebDriverSession(t, nil)
+	if err != nil {
+		t.Fatalf("webtest.NewWebDriverSession: %v", err)
+	}
+	// copybara:strip_end_and_replace_begin
+	// t.Fatalf("not yet implemented") // TODO: implement
+	// copybara:replace_end
+	return wd
+}
+
+func setup(t *testing.T) selenium.WebDriver {
+	wd := newWebdriver(t)
+	t.Cleanup(func() {
+		wd.Quit()
+	})
+
+	return wd
+}
+
+func renderHTML(t *testing.T, wd selenium.WebDriver, html string) {
+	if err := wd.Get("data:text/html;charset=utf-8," + html); err != nil {
+		t.Fatalf("wd.Get: %v", err)
+	}
+}
+
+func TestFindElements_FindsElements(t *testing.T) {
+	wd := setup(t)
+
+	tests := []struct {
+		desc    string
+		locator string
+		html    string
+	}{
+		{
+			desc:    "single quotes",
+			locator: "{button 'OK'}",
+			html:    "<button id='target'>OK</button>",
+		},
+		{
+			desc:    "double quotes",
+			locator: "{button \"OK\"}",
+			html:    "<button id='target'>OK</button>",
+		},
+		{
+			desc:    "nested elements",
+			locator: "{list} {listitem}",
+			html:    "<ul><li id='target'>foo</li></ul>",
+		},
+		{
+			desc:    "wildcard at start",
+			locator: "{button '*and_end'}",
+			html:    "<button id='target'>beginning_and_end</button>",
+		},
+		{
+			desc:    "wildcard at end",
+			locator: "{button 'beginning_and*'}",
+			html:    "<button id='target'>beginning_and_end</button>",
+		},
+		{
+			desc:    "wildcard at start and end",
+			locator: "{button '*and*'}",
+			html:    "<button id='target'>beginning_and_end</button>",
+		},
+		{
+			desc:    "outer list",
+			locator: "{region} outer {list}",
+			html:    "<div role='region'><ul id='target'><li><ul><li>foo</li></ul></li></ul></div>",
+		},
+		{
+			desc:    "unicode and control characters",
+			locator: "{button 'блČλñéç‪हिन्दी‬日本語‬‪한국어‬й‪ไ🤖-—–;|<>!\"_+'}",
+			html:    "<button id='target' aria-label='блČλñéç‪हिन्दी‬日本語‬‪한국어‬й‪ไ🤖-—–;|<>!&quot;_+'>OK</button>",
+		},
+		{
+			desc:    "escaped quotes",
+			locator: "{ button '\\'escaped quotes\\\\\\' and unescaped\\\\\\\\'}",
+			html:    "<button id='target'>'escaped quotes\\' and unescaped\\\\</button>",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			renderHTML(t, wd, test.html)
+			elm, err := FindElement(wd, test.locator, nil)
+			if err != nil {
+				t.Fatalf("semloc.FindElement: %v", err)
+			}
+			id, err := elm.GetAttribute("id")
+			if err != nil {
+				t.Fatalf("elm.GetAttribute: %v", err)
+			}
+			if id != "target" {
+				t.Errorf("wrong element selected, wanted element with id 'target'")
+			}
+		})
+	}
+}
+
+func TestFindElements_FindsDuplicates(t *testing.T) {
+	wd := setup(t)
+	renderHTML(t, wd, "<button>OK</button><button aria-label='OK'>foo</button><div role='button'>OK</div>")
+
+	elms, err := FindElements(wd, "{button 'OK'}", nil)
+	if err != nil {
+		t.Fatalf("FindElements: %v", err)
+	}
+	if len(elms) != 3 {
+		t.Errorf("semloc.FindElements returned %d elements, but wanted 3", len(elms))
+	}
+}
+
+func TestFindWithContext(t *testing.T) {
+	wd := setup(t)
+	renderHTML(t, wd, "<button>OK</button><div id='container'><button id='target'>OK</button></div>")
+
+	container, err := wd.FindElement(selenium.ByID, "container")
+	if err != nil {
+		t.Fatalf("FindElement(#container): %v", err)
+	}
+
+	elm, err := FindElement(wd, "{button 'OK'}", container)
+	if err != nil {
+		t.Fatalf("semloc.FindElements: %v", err)
+	}
+	id, err := elm.GetAttribute("id")
+	if err != nil {
+		t.Fatalf("elm.GetAttribute: %v", err)
+	}
+	if id != "target" {
+		t.Errorf("wrong element selected, wanted element with id 'target'")
+	}
+
+	elms, err := FindElements(wd, "{button 'OK'}", container)
+	if err != nil {
+		t.Fatalf("semloc.FindElements: %v", err)
+	}
+	if len(elms) != 1 {
+		t.Fatalf("semloc.FindElements returned %d elements, but expected 1", len(elms))
+	}
+	id, err = elms[0].GetAttribute("id")
+	if err != nil {
+		t.Fatalf("elm.GetAttribute: %v", err)
+	}
+	if id != "target" {
+		t.Errorf("wrong element selected, wanted element with id 'target'")
+	}
+}
+
+func TestInvalidSyntax(t *testing.T) {
+	wd := setup(t)
+	renderHTML(t, wd, "<button>OK</button><div id='container'><button id='target'>OK</button></div>")
+
+	tests := []struct {
+		desc    string
+		locator string
+	}{
+		{"unterminated", "{button 'OK'"},
+		{"bad escaping", "{button 'OK\\'}"},
+	}
+
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			_, err := FindElement(wd, test.locator, nil)
+			if got, want := getErrMsg(t, err), "invalid selector"; got != want {
+				t.Errorf("err returned by FindElement had message %q, but wanted %q", got, want)
+			}
+
+			_, err = FindElements(wd, test.locator, nil)
+			if got, want := getErrMsg(t, err), "invalid selector"; got != want {
+				t.Errorf("err returned by FindElements had message %q, but wanted %q", got, want)
+			}
+		})
+	}
+}
+
+func TestNoSuchElement(t *testing.T) {
+	wd := setup(t)
+	_, err := FindElement(wd, "{button 'this label does not exist'}", nil)
+	if got, want := getErrMsg(t, err), "no such element"; got != want {
+		t.Errorf("err returned by FindElements had message %q, but wanted %q", got, want)
+	}
+}
+
+func getErrMsg(t *testing.T, err error) string {
+	if err == nil {
+		return ""
+	}
+	var serr *selenium.Error
+	if !errors.As(err, &serr) {
+		t.Errorf("expected error to be a selenium.Error, but was %#v", err)
+	}
+	return serr.Err
+}
